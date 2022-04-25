53a0e673cc4474be8b0e2d96ee26b961ca4b8eae
Tran Nhu Tri <trannhutri0703@gmail.com>
2022-04-25 10:27:06 +0700

    raw html form

--- students/views.py
+++ students/views.py
@@ -3,14 +3,22 @@ from .forms import StudentForm
 from .models import Student
 # Create your views here.
 def create_view(request):
-    form = StudentForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-    
-    context = {
-        'form': form
-    }
+    if request.method == "POST":
+        new_code = request.POST.get("code")
+        print(new_code)
+        
+    context = {}
     return render(request, 'create.html', context)
+
+# def create_view(request):
+#     form = StudentForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+    
+#     context = {
+#         'form': form
+#     }
+#     return render(request, 'create.html', context)
 
 def detail_view(request):
     student = Student.objects.get(id=1)
